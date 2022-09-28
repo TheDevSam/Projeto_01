5b99caecef4690a9fa64ea6de86d0d542a5ec879
TheDevSam <106827797+TheDevSam@users.noreply.github.com>
2022-09-28 16:35:56 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php 
+
+	 session_start();
+
+	$autoload = function($class){
+		include('classes/'.$class.'.php');
+	};
+
+	spl_autoload_register($autoload);
+
+	define('INCLUDE_PATH','http://localhost/Projeto_01/');
+	define('INCLUDE_PATH_PAINEL',INCLUDE_PATH.'painel/');
+?>
