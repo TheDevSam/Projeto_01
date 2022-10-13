e065b4d0c1344e917387915c238ff8d1ac031877
TheDevSam <106827797+TheDevSam@users.noreply.github.com>
2022-10-13 20:22:09 -0300

    Add files via upload

--- painel/login.php
+++ painel/login.php
@@ -4,16 +4,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
     <link rel="stylesheet" href="<?php echo INCLUDE_PATH_PAINEL ?>css/style.css">
     <title>Login do Painel</title>
 </head>
 <body>
     
+
+    
     <div class="box-login">
-        <form>
+        <h2>Efetue o login:</h2>
+        <form method="post">
             <input type="text" name="user" placeholder="Login..." required/>
             <input type="password" name="password" placeholder="Senha..." required/>
             <input type="submit" name="acao" value="Logar" required/>
+            <a href="<?php echo INCLUDE_PATH?>">Voltar à página</a>
         </form>
     </div><!--box-login-->
 
